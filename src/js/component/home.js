72f6ba7881add787b21047e6403bef18edab56bf
Barbara Rincones <barbierincones@gmail.com>
2021-04-25 16:24:34 -0400

    First commit

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,51 @@
-import React from "react";
+import React, { useEffect, useState, useRef } from "react";
+import { Container, Row, Col, Button } from "react-bootstrap";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 export function Home() {
+	const [seconds, setSeconds] = useState(0);
+	const countRef = useRef(null);
+	useEffect(() => {
+		setInterval(() => setSeconds(prevSeconds => prevSeconds + 1), 1000);
+	}, []);
+
+	const handleReset = () => {
+		clearInterval(countRef.current);
+		setSeconds(0);
+	};
+
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<Container className="bg-dark text-white main" fluid>
+			<Row className="d-flex justify-content-center">
+				<Col className="clock" xs={1}>
+					<i className="far fa-clock"></i>
+				</Col>
+				<Col className="digit" xs={1}>
+					{Math.floor(seconds / 100000) % 10}
+				</Col>
+				<Col className="digit" xs={1}>
+					{Math.floor(seconds / 10000) % 10}
+				</Col>
+				<Col className="digit" xs={1}>
+					{Math.floor(seconds / 1000) % 10}
+				</Col>
+				<Col className="digit" xs={1}>
+					{Math.floor(seconds / 100) % 10}
+				</Col>
+				<Col className="digit" xs={1}>
+					{Math.floor(seconds / 10) % 10}
+				</Col>
+				<Col className="digit" xs={1}>
+					{seconds % 10}
+				</Col>
+				<Col className="reset" xs={1}>
+					<Button
+						className="button-reset"
+						variant="light"
+						onClick={handleReset}>
+						Reset
+					</Button>
+				</Col>
+			</Row>
+		</Container>
 	);
 }
